add08a585b50ec81a49bd6e7d8951582d4dce5eb
Artur Stępień <artur.stepien@jitsolutions.pl>
2016-11-12 14:26:49 +0100

    .

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -7,6 +7,8 @@ import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
+// Wee can assume that here should be production API
+
 const port = 3000;
 const app = express();
 const compile = webpack(config);
@@ -15,6 +17,14 @@ app.use(require('webpack-dev-middleware')(compile, {
 	noInfo: true,
 	publicPath: config.output.publicPath
 }));
+
+app.get('/users', function (req, res) {
+	res.json([
+		{ "id": 1, "firstName": "Mark", "lastName": "Smith", "emain": "em1@mail.com" },
+		{ "id": 2, "firstName": "Adam", "lastName": "Olivier", "emain": "em2@mail.com" },
+		{ "id": 3, "firstName": "Cory", "lastName": "Tylor", "emain": "em3@mail.com" }
+	])
+});
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
